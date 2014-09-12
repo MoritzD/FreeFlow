9d27fa753f65a315732274e5f1d22eab32821744
MoritzD <doetterl.moritz@gmail.com>
2014-09-12 12:34:37 +0000

    testmassage

--- IntelliJ/FreeFlow/src/com/example/FreeFlow/MainActivity.java
+++ IntelliJ/FreeFlow/src/com/example/FreeFlow/MainActivity.java
@@ -7,13 +7,13 @@ public class MainActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
-    int test1,test2;
+    int test1,test2,test3;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-
+    test1=2;
 
 
     }
