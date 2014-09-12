eef2ef1520ba0f2156d9212d1c36578a16153675
Sami Ede <sami.rockt@googlemail.com>
2014-09-12 14:25:46 +0000

    Imported TracePath Classes

---
+++ IntelliJ/FreeFlow/src/com/example/FreeFlow/Cellpath.java
@@ -0,0 +1,37 @@
+package com.example.FreeFlow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Sami on 05.09.14.
+ */
+
+public class Cellpath {
+
+    private ArrayList<Coordinate> m_path = new ArrayList<Coordinate>();
+
+    public void append( Coordinate co ) {
+        int idx = m_path.indexOf(  co );
+        if ( idx >= 0 ) {
+            for ( int i=m_path.size()-1; i > idx; --i ) {
+                m_path.remove(i);
+            }
+        }
+        else {
+            m_path.add(co);
+        }
+    }
+
+    public List<Coordinate> getCoordinates() {
+        return m_path;
+    }
+
+    public void reset() {
+        m_path.clear();
+    }
+
+    public boolean isEmpty() {
+        return m_path.isEmpty();
+    }
+}
--- IntelliJ/FreeFlow/src/com/example/FreeFlow/MainActivity.java
+++ IntelliJ/FreeFlow/src/com/example/FreeFlow/MainActivity.java
@@ -7,13 +7,11 @@ public class MainActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
-    int test1,test2,test3;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-    test1=2;
 
 
     }
