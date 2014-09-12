91192f2265864cb15fd1bc48473f2b00daba4348
MoritzD <doetterl.moritz@gmail.com>
2014-09-12 14:30:45 +0000

    testmassage

--- IntelliJ/FreeFlow/src/com/example/FreeFlow/MainActivity.java
+++ IntelliJ/FreeFlow/src/com/example/FreeFlow/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.FreeFlow;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
     /**
@@ -15,5 +18,13 @@ public class MainActivity extends Activity {
 
     test1=2;
 
+    }
+
+    private void buttonClick (View view){
+        Button but = (Button) view;
+        int id = but.getId();
+        if ( id == R.id.buttonPlay) {
+            startActivity(new Intent(this, PlayActivity.class));
+        }
     }
 }
---
+++ IntelliJ/FreeFlow/src/com/example/FreeFlow/PlayActivity.java
@@ -0,0 +1,17 @@
+package com.example.FreeFlow;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by moe on 12.09.14.
+ */
+public class PlayActivity extends Activity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+    }
+
+}
