d23efaed0ae858328c19a5477b19a767cb79b9c4
MoritzD <doetterl.moritz@gmail.com>
2014-09-23 20:23:30 +0000

    play aktivity

--- IntelliJ/FreeFlow/src/com/example/FreeFlow/PlayActivity.java
+++ IntelliJ/FreeFlow/src/com/example/FreeFlow/PlayActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -18,12 +19,19 @@ public class PlayActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.play);
 
+        Bundle b = getIntent().getExtras();
+        int challegeId = b.getInt("challengeId");
+        int puzzleId = b.getInt("puzzleId");
+
 
         //SharedPreferences settings = getSharedPreferences("ColorPref", MODE_PRIVATE);
 
         //int color = settings.getInt("pathColor", Color.CYAN);
 
         Board board = (Board) findViewById(R.id.board);
+        board.setLevel(Global.getInstance().mChallenge.get(challegeId).mPuzzle.get(puzzleId));
+
+        //board.loadLevel(Global.getInstance().mChallenge.get(0).mPuzzle.get(0));
         //board.setColor(color);
 
     }
