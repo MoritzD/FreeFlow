63a2233f6582f3f8a4d2727dd1498d790cf59089
MoritzD <doetterl.moritz@gmail.com>
2014-09-23 13:19:31 +0000

    bla bla

--- IntelliJ/FreeFlow/src/com/example/FreeFlow/ChallengeList.java
+++ IntelliJ/FreeFlow/src/com/example/FreeFlow/ChallengeList.java
@@ -2,8 +2,11 @@ package com.example.FreeFlow;
 
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by Sami on 22.09.14.
@@ -20,6 +23,17 @@ public class ChallengeList extends ListActivity {
 
         setListAdapter(adapter);
 
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id){
+        //startActivity(new Intent(getApplicationContext(), PlayActivity.class));
+
+        Intent intent = new Intent(getApplicationContext(), PlayActivity.class);
+        Bundle b = new Bundle();
+        b.putInt("key", position); //Your id
+        intent.putExtras(b); //Put your id to your next Intent
+        startActivity(intent);
     }
 
 
