e132acc0993e1a248c1f0dbd7e9dd980dd260157
Sami Ede <sami.rockt@googlemail.com>
2014-09-23 20:22:44 +0000

    Adapter

--- IntelliJ/FreeFlow/src/com/example/FreeFlow/ChallengeAdapter.java
+++ IntelliJ/FreeFlow/src/com/example/FreeFlow/ChallengeAdapter.java
@@ -1,15 +1,12 @@
 package com.example.FreeFlow;
 
-import android.app.ListActivity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.lang.reflect.Array;
 import java.util.List;
 
 /**
