47618e4152124ad6cd51bc15eef6a01d53ad3680
Sami Ede <sami.rockt@googlemail.com>
2014-09-23 20:26:07 +0000

    Adapter

--- IntelliJ/FreeFlow/src/com/example/FreeFlow/PuzzleGrid.java
+++ IntelliJ/FreeFlow/src/com/example/FreeFlow/PuzzleGrid.java
@@ -2,6 +2,7 @@ package com.example.FreeFlow;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -26,7 +27,7 @@ public class PuzzleGrid extends Activity {
         mContext = getApplicationContext();
 
         Bundle b = getIntent().getExtras();
-        int cId = b.getInt("id");
+        final int cId = b.getInt("id");
         for(int i = 0; i < mGlobal.mChallenge.size(); i++){
             if(mGlobal.mChallenge.get(i).mId == cId){
                 mChallenge = mGlobal.mChallenge.get(i);
@@ -35,7 +36,7 @@ public class PuzzleGrid extends Activity {
 
 
 
-        GridView gridview = (GridView) findViewById(R.id.gridview);
+        final GridView gridview = (GridView) findViewById(R.id.gridview);
 
 
 
@@ -48,6 +49,15 @@ public class PuzzleGrid extends Activity {
 
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+
+                Intent intent = new Intent(getApplicationContext(), PlayActivity.class);
+                Bundle b = new Bundle();
+                b.putInt("challegeId",cId);
+
+                Puzzle clicked = (Puzzle) gridview.getItemAtPosition(position);
+                b.putInt("puzzleId", Integer.parseInt(clicked.getId()));
+                startActivity(intent);
+
             }
         });
     }
