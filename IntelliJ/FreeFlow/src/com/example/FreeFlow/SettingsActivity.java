8982b772eefbc95624c4836c8293e2c8b1d3a91f
MoritzD <doetterl.moritz@gmail.com>
2014-09-24 13:38:49 +0000

    puzzle!

---
+++ IntelliJ/FreeFlow/src/com/example/FreeFlow/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.example.FreeFlow;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by moe on 24.09.14.
+ */
+public class SettingsActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+    }
+}
